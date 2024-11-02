ad00a19d2827c483b912da0a1e0d9e7f36914d95
joonsoo <junsu_01@naver.com>
2024-11-02 22:11:29 +0900

    type에 해당하는 drink 호출 구현 (test해야함)

--- Backend/src/main/java/com/example/backend/repository/DrinkRepository.java
+++ Backend/src/main/java/com/example/backend/repository/DrinkRepository.java
@@ -1,10 +1,15 @@
 package com.example.backend.repository;
 
+import com.example.backend.entity.Drink;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-public interface DrinkRepository extends JpaRepository {
+@Repository
+public interface DrinkRepository extends JpaRepository <Drink, Integer> {
+
+    List<Drink> findByType(String type); // 타입에 해당하는 drink 반환
 }
