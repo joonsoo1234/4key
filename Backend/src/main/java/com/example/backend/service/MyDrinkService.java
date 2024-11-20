7747694ea48cef7d12c234b3f4a391ceb3ecfc60
joonsoo <junsu_01@naver.com>
2024-11-20 23:29:30 +0900

    기능 테스트용 문장 추가

--- Backend/src/main/java/com/example/backend/service/MyDrinkService.java
+++ Backend/src/main/java/com/example/backend/service/MyDrinkService.java
@@ -6,12 +6,16 @@ import com.example.backend.entity.MyDrink;
 import com.example.backend.repository.DrinkRepository;
 import com.example.backend.repository.MyDrinkRepository;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class MyDrinkService {
+
+    private static final Logger logger = LoggerFactory.getLogger(MyDrinkService.class); //log test
 
     private final MyDrinkRepository myDrinkRepository;
     private final DrinkRepository drinkRepository;
@@ -20,6 +24,7 @@ public class MyDrinkService {
     public MyDrink addDrinkToCart(MyDrink request) {
         // 1. 선택한 음료 정보 가져오기
         Drink drink = drinkRepository.findOneById(request.getDrink().getId());
+        logger.info(drink.toString());
 
         // 2. 가격 계산 (1개 샷 500원 추가)
         int shotPrice = 500;
