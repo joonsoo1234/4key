72f1eed8b45b455939bc79e184119fd5c46e273c
hyun <joshuahong@naver.com>
2024-11-21 19:20:47 +0900

    메인화면, 카페화면 만듦

---
+++ frontend/src/components/Navbar.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import '../styles/layout.css';
+import { AiFillHome } from 'react-icons/ai';
+import { RiMenuFill } from 'react-icons/ri';
+
+function Navbar() {
+    return (
+        <header>
+            <nav className="navbar">
+                <Link to="/" className="home-button">
+                    <AiFillHome size={24} />
+                </Link>
+                <h1 className="navbar-title">Cafe Kiosk</h1>
+                <Link to="/menu" className="menu-button">
+                    <RiMenuFill size={24} />
+                </Link>
+            </nav>
+        </header>
+    );
+}
+
+export default Navbar;
