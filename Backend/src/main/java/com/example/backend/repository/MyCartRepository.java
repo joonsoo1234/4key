6611897e79ed2a6296876b3edf536b2da0d7d3cb
joonsoo <junsu_01@naver.com>
2024-12-17 14:55:11 +0900

    장바구니에서 +,-로 수량 수정 기능 구현완 갯수 0이 되면 해당 id 데이터를 mycart DB에서 제거

--- Backend/src/main/java/com/example/backend/repository/MyCartRepository.java
+++ Backend/src/main/java/com/example/backend/repository/MyCartRepository.java
@@ -1,9 +1,11 @@
 package com.example.backend.repository;
 
+import com.example.backend.entity.Item;
 import com.example.backend.entity.MyCart;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface MyCartRepository extends JpaRepository<MyCart, Integer> {
+    MyCart findOneById(int id); //해당 id 유무 판단
 }
